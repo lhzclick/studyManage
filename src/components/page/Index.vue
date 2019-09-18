d26acc7f250ecaf94631a51d9cbd03d2812853da
liuhu <392539910@qq.com>
2019-09-18 17:54:33 +0800

    study系统 前端vue首次提交

---
+++ src/components/page/Index.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <el-row :gutter="20">
+      <el-col :span="8">
+        <el-card shadow="hover" class="mgb20" style="height:252px;">
+        </el-card>
+      </el-col>
+      <el-col :span="8">
+        <el-card shadow="hover" class="mgb20" style="height:252px;">
+        </el-card>
+      </el-col>
+    </el-row>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index",
+  data() {
+    return {}
+  },
+  components: {
+  },
+  computed: {
+    
+  },
+  created() {
+    
+    this.getData()
+  },
+  activated() {
+    
+  },
+  deactivated() {
+   
+  },
+  methods: {
+    getData() {
+      this.$axios({
+        method: "post",
+        url: "/getScore",
+        data: {
+          pageNo:2,
+					pageSize:3,
+					type:'数学一单元测试'
+        }
+      }).then(res => {
+        console.log(res);
+      });
+    }
+  }
+};
+</script>
+
+
+<style scoped>
+.el-row {
+  margin-bottom: 20px;
+}
+
+
+
+
+</style>
